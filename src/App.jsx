625b2474e4d9f7367b6d734e02ac4d4463e67147
Raiyan27 <abdullahalraiyan4@gmail.com>
2024-10-24 20:24:40 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [balance, setBalance] = useState(0);
@@ -11,6 +12,7 @@ const App = () => {
     <div className="bg-base-100 min-h-screen">
       <Header balance={balance} />
       <Banner creditHandle={handleClaimCredit} />
+      <Footer />
     </div>
   );
 };
